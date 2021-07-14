bf2cf5ea46f1fea14feda829cc78fa199a0c8898
Vasa <vasilylyapunov@gmail.com>
2021-07-13 20:14:39 -0600

    Changed HTML parsing to be more universal

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import re
+
 input = '''
-<tr>
-                        <td id="bottom-inline-score">4</td>
-                        <td id="left-inline-score">24</td>
-                        <td id="top-inline-score">23</td>
-                        <td id="right-inline-score">27</td>
+<tbody>
+                    <tr>
+                        <th colspan="4">Total game score</th>
                     </tr>
+                    <tr class="names-row">
+                        <th class="bottom-player-name">Vasa</th>
+                        <th class="left-player-name">Mike</th>
+                        <th class="top-player-name">Bill</th>
+                        <th class="right-player-name">Lisa</th>
+                    </tr>
+                    <tr>
+                        <td id="bottom-inline-score">0</td>
+                        <td id="left-inline-score">0</td>
+                        <td id="top-inline-score">0</td>
+                        <td id="right-inline-score">0</td>
+                    </tr>
+                </tbody>
+'''
+def main():
+    _, _, names, scores = input.split('<tr')
+    print(1,names)
+    print(2,scores)
+
+    names = getHTMLContents(names)
+    scores = getHTMLContents(scores, op=lambda x: int(x))
+    print(2,scores)
+    print(3,names)
+
+def getHTMLContents(str, op=lambda x: x):
+    items = re.findall('(?<=>).*(?=<)', str)
+    output_list = [op(x) for x in items]
+    return output_list
+
+main()
 '''
 input = input.split('/')
 l = []
 j = 0
 for i in input:
-    a = 'lkdfhisoe78347834 (())&/&745  '
     result = re.sub('[^0-9]','', i)
     numb = int(result)
     if j==0:
@@ -31,3 +59,4 @@ people = ['vasa','sam','mike','jon']
 
 for i in range(4):
     print(people[i], l[i])
+'''
