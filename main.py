125687d3cef18924ce95ce19cbf72cd03f3db7cc
Vasa <vasilylyapunov@gmail.com>
2021-07-13 20:40:59 -0600

    Completed scoring capabilities

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import re
 
-input = '''
+
+def main(current_scores, html_data):
+    _, _, names, scores = html_data.split('<tr')
+    print(1, names)
+    print(2, scores)
+
+    names = get_html_contents(names)
+    scores = get_html_contents(scores, op=lambda x: int(x))
+    print(2, scores)
+    print(3, names)
+
+    new_scores = dict(zip(names, scores))
+    for name in names:
+        if name in current_scores:
+            current_scores[name] = current_scores[name] + new_scores[name]
+        else:
+            current_scores[name] = new_scores[name]
+
+    print(current_scores)
+    current_scores = sorted(current_scores.items(), key=lambda x: x[1], reverse=True)
+    print(current_scores)
+    place = 1
+    for score in current_scores:
+        print('{}. {}: \t{}'.format(place, score[0], score[1]))
+        place = place + 1
+
+
+def get_html_contents(str, op=lambda x: x):
+    items = re.findall('(?<=>).*(?=<)', str)
+    output_list = [op(x) for x in items]
+    return output_list
+
+
+html_data = '''
 <tbody>
                     <tr>
                         <th colspan="4">Total game score</th>
@@ -12,51 +45,18 @@ input = '''
                         <th class="right-player-name">Lisa</th>
                     </tr>
                     <tr>
-                        <td id="bottom-inline-score">0</td>
+                        <td id="bottom-inline-score">1</td>
                         <td id="left-inline-score">0</td>
                         <td id="top-inline-score">0</td>
                         <td id="right-inline-score">0</td>
                     </tr>
                 </tbody>
 '''
-def main():
-    _, _, names, scores = input.split('<tr')
-    print(1,names)
-    print(2,scores)
-
-    names = getHTMLContents(names)
-    scores = getHTMLContents(scores, op=lambda x: int(x))
-    print(2,scores)
-    print(3,names)
-
-def getHTMLContents(str, op=lambda x: x):
-    items = re.findall('(?<=>).*(?=<)', str)
-    output_list = [op(x) for x in items]
-    return output_list
-
-main()
-'''
-input = input.split('/')
-l = []
-j = 0
-for i in input:
-    result = re.sub('[^0-9]','', i)
-    numb = int(result)
-    if j==0:
-        numb = numb + 113
-    if j==1:
-        numb = numb + 119
-    if j==2:
-        numb = numb + 147
-    if j==3:
-        numb = numb + 167
-    l.append(numb)
-    j = j+1
-    if j==4:
-        break
-
-people = ['vasa','sam','mike','jon']
-
-for i in range(4):
-    print(people[i], l[i])
-'''
+current_scores = {
+    'Vasa': 117,
+    'Sam': 143,
+    'Jon': 194,
+    'Mike': 170
+}
+protected_names = ['Vasa', 'Sam', 'Jon']
+main(current_scores, html_data)
