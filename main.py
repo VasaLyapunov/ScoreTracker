5f423aa6ed084f27bce9f2317963b1415bf069af
Vasa <vasilylyapunov@gmail.com>
2021-07-13 21:03:44 -0600

    Added ability to merge scores of AI players

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import re
 
 
-def main(current_scores, html_data):
+def main(current_scores, html_data, protected_names, merge_ai=True):
     _, _, names, scores = html_data.split('<tr')
-    print(1, names)
-    print(2, scores)
-
     names = get_html_contents(names)
     scores = get_html_contents(scores, op=lambda x: int(x))
-    print(2, scores)
-    print(3, names)
 
     new_scores = dict(zip(names, scores))
     for name in names:
@@ -18,22 +13,48 @@ def main(current_scores, html_data):
         else:
             current_scores[name] = new_scores[name]
 
-    print(current_scores)
-    current_scores = sorted(current_scores.items(), key=lambda x: x[1], reverse=True)
-    print(current_scores)
-    place = 1
-    for score in current_scores:
-        print('{}. {}: \t{}'.format(place, score[0], score[1]))
-        place = place + 1
+    # Combine all non-protected names into one overall AI score
+    if merge_ai:
+        current_scores = merge_ai_scores(current_scores, protected_names)
+
+    current_scores = sorted(current_scores.items(), key=lambda x: x[1])
+    print_scores(current_scores)
 
 
+# Given HTML data grabbed off of a table of scores, extract data into a list
 def get_html_contents(str, op=lambda x: x):
     items = re.findall('(?<=>).*(?=<)', str)
     output_list = [op(x) for x in items]
     return output_list
 
 
-html_data = '''
+def merge_ai_scores(scores, protected_names):
+    protected_names.append('AI')
+    new_scores = {'AI': 0}
+    for k, v in scores.items():
+        if k not in protected_names:
+            new_scores['AI'] = new_scores['AI'] + scores[k]
+        else:
+            new_scores[k] = scores[k]
+    return new_scores
+
+
+# Print formatted scores, given a list of tuples
+def print_scores(scores):
+
+    max_len = 0
+    for data in scores:
+        max_len = max(max_len, len(data[0]))
+
+    place = 1
+    for data in scores:
+        name = data[0]
+        score = data[1]
+        print('{}. {}: {}{}'.format(place, name, ' '*(max_len - len(name)), score))
+        place = place + 1
+
+
+_html_data = '''
 <tbody>
                     <tr>
                         <th colspan="4">Total game score</th>
@@ -52,11 +73,11 @@ html_data = '''
                     </tr>
                 </tbody>
 '''
-current_scores = {
+_current_scores = {
     'Vasa': 117,
     'Sam': 143,
     'Jon': 194,
     'Mike': 170
 }
-protected_names = ['Vasa', 'Sam', 'Jon']
-main(current_scores, html_data)
+_protected_names = ['Vasa', 'Sam', 'Jon']
+main(_current_scores, _html_data, _protected_names)
